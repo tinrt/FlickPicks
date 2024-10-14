3a409a1e1a1cb1f4d5c7b1027e9436b1519b76df
Tina <nosratitina@gmail.com>
2024-10-14 19:41:12 -0400

    User input handled

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from MovieData import get_movie_info
 from Review_Summary import generate_movie_summary
 from Content_req_test import recommend_movies as content_req
@@ -6,41 +6,41 @@ from User_req import recommend_movies as user_req
 import os
 import pickle
 
-'''
-Author: Tina Nosrati
-Last Update: 10/14/2024
-
-Description: 
-This script will be the main Flask app
-'''
-
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     try:
-        # getting movie info
-        name = "Jarhead"
-        df = get_movie_info(name)
+        name = None
+        info_dict, summary, content_req_list, user_req_list = None, None, None, None
 
-        # create dictionary of movie information
-        info_dict = df.iloc[0].to_dict()
+        if request.method == "POST":
+            # Get the movie name from the form
+            name = request.form.get('name')
 
-        # generate review summary
-        summary = generate_movie_summary(name)
+            if name:
+                # Get movie info
+                df = get_movie_info(name)
 
-        # content-based recommendation
-        content_model_path = os.path.expanduser('~/content_req_model.pkl')
-        with open(content_model_path, 'rb') as f:
-            df, cosine_sim, tfidf = pickle.load(f)
-        
-        content_req_list = content_req(name, df, cosine_sim)
+                # Create dictionary of movie information
+                info_dict = df.iloc[0].to_dict()
 
-        # user-based recommendation system
-        user_req_list = user_req(name)
+                # Generate review summary
+                summary = generate_movie_summary(name)
+
+                # Content-based recommendation
+                content_model_path = os.path.expanduser('~/content_req_model.pkl')
+                with open(content_model_path, 'rb') as f:
+                    df, cosine_sim, tfidf = pickle.load(f)
+                
+                content_req_list = content_req(name, df, cosine_sim)
+
+                # User-based recommendation system
+                user_req_list = user_req(name)
 
         return render_template(
             "home.html",
+            name=name,
             info=info_dict,
             summary=summary,
             content_req_list=content_req_list,
